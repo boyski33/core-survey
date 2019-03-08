5765619020c5b056e572a8dead39c5ca8892d41c
Boyan Kushlev <takarabukara@gmail.com>
2019-03-08 19:03:31 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Core Survey microservice
+Hippo Survey platform microservice for handling creation and retrieval of surveys
+
---
+++ src/test/java/com/hippo/coresurvey/CoreSurveyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hippo.coresurvey;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoreSurveyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
