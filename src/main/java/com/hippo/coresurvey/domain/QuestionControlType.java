e29be239f302a3af8b413bdd28e958be1a86438c
Boyan Kushlev <takarabukara@gmail.com>
2019-03-16 00:12:32 +0200

    add question objects

--- src/main/java/com/hippo/coresurvey/domain/QuestionControlType.java
+++ src/main/java/com/hippo/coresurvey/domain/QuestionControlType.java
@@ -1,6 +1,39 @@
 package com.hippo.coresurvey.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public enum QuestionControlType {
   TEXTBOX,
-  DROPDOWN
+  DROPDOWN,
+  RADIO,
+  CHECKBOX;
+
+  private static Map<String, QuestionControlType> namesMap = new HashMap<>();
+
+  static {
+    namesMap.put("textbox", TEXTBOX);
+    namesMap.put("dropdown", DROPDOWN);
+    namesMap.put("radio", RADIO);
+    namesMap.put("checkbox", CHECKBOX);
+  }
+
+  @JsonCreator
+  public static QuestionControlType forValue(String name) {
+    return namesMap.get(name.toLowerCase());
+  }
+
+  @JsonValue
+  public String toValue() {
+    for (Map.Entry<String, QuestionControlType> entry : namesMap.entrySet()) {
+      if (entry.getValue() == this) {
+        return entry.getKey();
+      }
+    }
+
+    return null;
+  }
 }
