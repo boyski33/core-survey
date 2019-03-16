5a53beba9aadf0300c6d6d34767cad74a453afaf
Boyan Kushlev <takarabukara@gmail.com>
2019-03-16 21:11:10 +0200

    add tests for CollectionsUtil class

--- src/test/java/com/hippo/coresurvey/domain/SurveyServiceTest.java
+++ src/test/java/com/hippo/coresurvey/domain/SurveyServiceTest.java
@@ -3,12 +3,8 @@ package com.hippo.coresurvey.domain;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
-import static java.util.Arrays.asList;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -21,8 +17,8 @@ public class SurveyServiceTest {
     this.surveyService = new SurveyService(new MockSurveyRepository());
   }
 
-  @Test
-  public void given_metadata_flag_true_when_get_surveys_then_return_empty_question_list() {
+  @Test public void
+  given_metadata_flag_true_when_get_surveys_then_return_empty_question_list() {
     //GIVEN
     boolean requestMetadata = true;
 
@@ -33,8 +29,8 @@ public class SurveyServiceTest {
     assertTrue(actual.get(0).getQuestions().isEmpty());
   }
 
-  @Test
-  public void given_metadata_flag_false_when_get_surveys_then_return_with_questions() {
+  @Test public void
+  given_metadata_flag_false_when_get_surveys_then_return_with_questions() {
     //GIVEN
     boolean requestMetadata = false;
 
@@ -46,32 +42,4 @@ public class SurveyServiceTest {
   }
 
 
-  private class MockSurveyRepository implements SurveyRepository {
-
-    @Override
-    public List<Survey> getAllSurveys() {
-      return new ArrayList<>(asList(
-          new Survey("id", "title", "description", asList(new Question())),
-          new Survey("id2", "title2", "description2", asList(new Question()))
-      ));
-    }
-
-    @Override
-    public List<Survey> getAllSurveysMetadata() {
-      return new ArrayList<>(asList(
-          new Survey("id", "title", "description", Collections.emptyList()),
-          new Survey("id2", "title2", "description2", Collections.emptyList())
-      ));
-    }
-
-    @Override
-    public Optional<Survey> getSurveyById(String id) {
-      return Optional.empty();
-    }
-
-    @Override
-    public Survey addSurvey(Survey survey) {
-      return null;
-    }
-  }
 }
