4b5043567a5c573ad22dcced04e2fa2d06d677d4
Boyan Kushlev <takarabukara@gmail.com>
2019-03-16 21:00:28 +0200

    add tests for SurveyController and SurveyService

--- src/test/java/com/hippo/coresurvey/domain/SurveyServiceTest.java
+++ src/test/java/com/hippo/coresurvey/domain/SurveyServiceTest.java
@@ -1,7 +1,77 @@
 package com.hippo.coresurvey.domain;
 
-import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class SurveyServiceTest {
 
+  private SurveyService surveyService;
+
+  @Before
+  public void setup() {
+    this.surveyService = new SurveyService(new MockSurveyRepository());
+  }
+
+  @Test
+  public void given_metadata_flag_true_when_get_surveys_then_return_empty_question_list() {
+    //GIVEN
+    boolean requestMetadata = true;
+
+    //WHEN
+    List<Survey> actual = surveyService.getAllSurveys(requestMetadata);
+
+    //THEN
+    assertTrue(actual.get(0).getQuestions().isEmpty());
+  }
+
+  @Test
+  public void given_metadata_flag_false_when_get_surveys_then_return_with_questions() {
+    //GIVEN
+    boolean requestMetadata = false;
+
+    //WHEN
+    List<Survey> actual = surveyService.getAllSurveys(requestMetadata);
+
+    //THEN
+    assertFalse(actual.get(0).getQuestions().isEmpty());
+  }
+
+
+  private class MockSurveyRepository implements SurveyRepository {
+
+    @Override
+    public List<Survey> getAllSurveys() {
+      return new ArrayList<>(asList(
+          new Survey("id", "title", "description", asList(new Question())),
+          new Survey("id2", "title2", "description2", asList(new Question()))
+      ));
+    }
+
+    @Override
+    public List<Survey> getAllSurveysMetadata() {
+      return new ArrayList<>(asList(
+          new Survey("id", "title", "description", Collections.emptyList()),
+          new Survey("id2", "title2", "description2", Collections.emptyList())
+      ));
+    }
+
+    @Override
+    public Optional<Survey> getSurveyById(String id) {
+      return Optional.empty();
+    }
+
+    @Override
+    public Survey addSurvey(Survey survey) {
+      return null;
+    }
+  }
 }
---
+++ src/test/java/com/hippo/coresurvey/web/rest/SurveyControllerTest.java
@@ -0,0 +1,55 @@
+package com.hippo.coresurvey.web.rest;
+
+import com.hippo.coresurvey.domain.Survey;
+import com.hippo.coresurvey.domain.SurveyService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+public class SurveyControllerTest {
+  @Mock
+  private SurveyService service;
+
+  @InjectMocks
+  private SurveyController controller;
+
+  @Before
+  public void setUp() throws Exception {
+
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void given_invalid_id_when_calling_survey_endpoint_then_return_404() {
+    // GIVEN
+    when(service.getSurveyById(any())).thenReturn(Optional.empty());
+
+    // WHEN
+    ResponseEntity res = controller.getSurveyById("1");
+
+    // THEN
+    assertEquals(HttpStatus.NOT_FOUND, res.getStatusCode());
+  }
+
+  @Test
+  public void given_valid_id_when_calling_survey_endpoint_then_return_200() {
+    // GIVEN
+    when(service.getSurveyById(any())).thenReturn(Optional.of(new Survey()));
+
+    // WHEN
+    ResponseEntity res = controller.getSurveyById("1");
+
+    // THEN
+    assertEquals(HttpStatus.OK, res.getStatusCode());
+  }
+}
