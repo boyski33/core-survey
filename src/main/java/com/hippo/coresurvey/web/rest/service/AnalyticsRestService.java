4c6cd8a2575ee2417f47ad1e006246aa7f49bc73
Boyan Kushlev <boyan.kushlev@paysafe.com>
2019-04-10 14:23:19 +0300

    add analytics rest service implementation

---
+++ src/main/java/com/hippo/coresurvey/web/rest/service/AnalyticsRestService.java
@@ -0,0 +1,8 @@
+package com.hippo.coresurvey.web.rest.service;
+
+import com.hippo.coresurvey.domain.analytics.AnalyticsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnalyticsRestService implements AnalyticsService {
+}
